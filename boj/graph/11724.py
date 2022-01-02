4af6172c94bee54cc3c7555bfba0d38badf4d9ba
Jihoon Park <chad9349@gmail.com>
2022-01-02 21:31:09 +0900

    algorithm study 22.01.02
    boj graph

--- boj/graph/11724.py
+++ boj/graph/11724.py
@@ -1,19 +1,21 @@
+import sys 
+input = sys.stdin.readline
 
-def getParent(parent, a):
+def find(parent, a):
     if parent[a] != a:
-        parent[a] = getParent(parent, parent[a])
+        parent[a] = find(parent, parent[a])
     return parent[a]
 
-def composeParent(parent, a, b):
-    aP = getParent(parent, a)
-    bP = getParent(parent, b)
+def union(parent, a, b):
+    aP = find(parent, a)
+    bP = find(parent, b)
 
+    if aP == bP:
+        return
     if aP < bP:
         parent[bP] = aP
     else:
         parent[aP] = bP
-
-
 
 n, m = map(int, input().split())
 
@@ -25,12 +27,44 @@ for i in range(1, n + 1):
 graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    if getParent(parent, a) == getParent(parent, b):
-        continue
-    composeParent(parent, a, b)
+    union(parent, a, b)
 
-parentSet = set()
+count = 0
 for i in range(1, n + 1):
-    parentSet.add(parent[i])
+    if parent[i] == i:
+        count += 1
 
-print(len(parentSet))
+print(count)
+
+
+# dfs 로 풀기
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+visited = [0] * (n + 1)
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+count = 0
+
+for i in range(1, n + 1):
+    if visited[i] != 0:
+        continue
+
+    count += 1
+    stack = []
+    stack.append(i)
+    while stack:
+        now = stack.pop()
+        if visited[now] == 0:
+            visited[now] = 1
+            for node in graph[now]:
+                stack.append(node)
+
+print(count)
